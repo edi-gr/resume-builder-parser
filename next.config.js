61d046379f3ebc574b323262a2e4e3b958cc966b
Edson <101560941+edi-gr@users.noreply.github.com>
2024-09-14 23:09:57 +0530

    done

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  output: "standalone",
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
